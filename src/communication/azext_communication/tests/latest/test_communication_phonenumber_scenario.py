7efa4e0a763273df958571b4fea3b329b22e89a6
maulinasharma <maulina.sharma@hcl.com>
2021-11-18 13:21:37 +0530

    Added test cases for communication commands

---
+++ src/communication/azext_communication/tests/latest/test_communication_phonenumber_scenario.py
@@ -0,0 +1,39 @@
+from azure.cli.testsdk import ScenarioTest, ResourceGroupPreparer, CommunicationResourcePreparer
+from .utils import get_test_source_phonenumber, get_new_phonenumber
+import os
+
+class CommunicationPhonenumberScenarios(ScenarioTest):
+
+    @ResourceGroupPreparer(name_prefix='clitestcommunication_MyResourceGroup'[:7], key='rg', parameter_name='rg')
+    @CommunicationResourcePreparer(resource_group_parameter_name='rg')
+    def test_list_phonenumbers(self, communication_resource_info):
+        print("------self.is_live--------")
+        print(self.is_live)
+        if self.is_live:
+            get_new_phonenumber(communication_resource_info[1])
+            os.environ['AZURE_COMMUNICATION_CONNECTION_STRING'] = communication_resource_info[1]
+
+        phonenumber_list = self.cmd('az communication phonenumbers list-phonenumbers').get_output_in_json()
+        assert len(phonenumber_list) > 0
+
+
+    @ResourceGroupPreparer(name_prefix='clitestcommunication_MyResourceGroup'[:7], key='rg', parameter_name='rg')
+    @CommunicationResourcePreparer(resource_group_parameter_name='rg')
+    def test_show_phonenumbers(self, communication_resource_info):
+        phonenumber = get_test_source_phonenumber(self.is_live)
+        print("------self.is_live--------")
+        print(self.is_live)
+        if phonenumber is None:
+            phonenumber = get_new_phonenumber(communication_resource_info[1])
+
+        self.kwargs.update({'phonenumber' : phonenumber})
+
+        if self.is_live:
+            os.environ['AZURE_COMMUNICATION_CONNECTION_STRING'] = communication_resource_info[1]
+
+        phonenumber_info = self.cmd('az communication phonenumbers show-phonenumber --phonenumber \"{phonenumber}\"').get_output_in_json() 
+        self.assertIsNotNone(phonenumber_info['id'])
+        self.assertIsNotNone(phonenumber_info['assignmentType'])
+        self.assertIsNotNone(phonenumber_info['capabilities'])
+        self.assertIsNotNone(phonenumber_info['cost'])
+        self.check(phonenumber_info['phoneNumber'], phonenumber)
